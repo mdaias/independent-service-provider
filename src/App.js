2fe06a0a4ac054614520e8b2cbda48893057c926
mdaias <mdaias29@gmail.com>
2022-04-18 09:25:35 +0600

    added navbar and create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Services from './components/Home/Services/Services';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
-    <div className="App">
-        <h1>hello world</h1>
+    <div>
+        <Header></Header>
+        <Routes>
+
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+
+        </Routes>
+       
+        <Footer></Footer>
     </div>
   );
 }
